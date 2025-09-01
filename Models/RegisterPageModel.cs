1d601bc5ef06408242486e08ee979c8cd336e426
manifold21 <paulbamo7@gmail.com>
2025-09-02 02:15:08 +0800

    MVVM2

--- Models/RegisterPageModel.cs
+++ Models/RegisterPageModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,33 @@ namespace Coftea_Capstone.Models
 {
     internal class RegisterPageModel
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+
+
+        [Unique, NotNull]
+        public string Email { get; set; } = string.Empty;
+
+        [NotNull]
+        public string FirstName { get; set; } = string.Empty;
+
+        [NotNull]
+        public string LastName { get; set; } = string.Empty;
+
+        [NotNull]
+        public string Password { get; set; } = string.Empty;
+
+        [NotNull]
+        public string Role { get; set; } = string.Empty;
+
+        [NotNull]
+        public DateTime Birthday { get; set; }
+
+        [NotNull]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [NotNull]
+        public string Address { get; set; } = string.Empty;
     }
 }
